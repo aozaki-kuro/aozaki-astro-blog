37ceb4e5b2d30e37b20635aa8b4c2694a526795b
Aozaki <i@aozaki.cc>
2025-07-02 19:24:13 +0800

    new: add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -4,9 +4,11 @@ import icon from 'astro-icon'
 
 import tailwind from '@astrojs/tailwind'
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.aozaki.cc',
 
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), sitemap()],
 })
