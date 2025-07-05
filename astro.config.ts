a00c4d74a37b615a17039866b9bf32fa06b31f44
Aozaki <i@aozaki.cc>
2025-07-06 06:46:55 +0800

    fix: v4 compatible with dark mode switch

--- astro.config.ts
+++ astro.config.ts
@@ -2,17 +2,21 @@ import { defineConfig } from 'astro/config'
 
 import icon from 'astro-icon'
 
-import tailwind from '@astrojs/tailwind'
-
 import sitemap from '@astrojs/sitemap'
+
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.aozaki.cc',
   output: 'static',
+
   integrations: [
-    tailwind(),
     icon(),
     sitemap({ filter: page => page !== 'https://blog.aozaki.cc/photography/' }),
   ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
